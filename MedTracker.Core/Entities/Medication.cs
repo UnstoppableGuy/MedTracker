b1fa2e3080ee590079ed38ae59bd66ee10783e2c
UnstoppableGuy <pavel.takynov@gmail.com>
2025-07-28 18:21:20 +0300

    main entities added

---
+++ MedTracker.Core/Entities/Medication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTracker.Core.Entities
+{
+    public class Medication
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Form { get; set; } = string.Empty;
+        public string Dosage { get; set; } = string.Empty;
+        public List<MedicationSchedule> Schedules { get; set; } = [];
+    }
+}
